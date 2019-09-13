1dc35971d398e86eedbf80e3575d60605894922f
Alex <blohinalexej@mail.ru>
2019-09-13 16:37:08 +0300

    init, common functionality

---
+++ app.py
@@ -0,0 +1,114 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://" +\
+    "madma:mdma@localhost:5432/app"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+# Many-to-many. Association table
+users_jokes = db.Table(
+    "users_jokes",
+    db.Column(
+        'user_id', db.Integer,
+        db.ForeignKey('users.id'),
+        primary_key=True
+    ),
+    db.Column(
+        'joke_id', db.Integer,
+        db.ForeignKey('jokes.id'),
+        primary_key=True
+    )
+)
+
+
+class Joke(db.Model):
+    __tablename__ = "jokes"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return "<Joke (id=%d, content=%s)>"\
+            % (self.id, self.content)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), nullable=False)
+    my_jokes = db.relationship(
+        "Joke", secondary=users_jokes,
+        backref="users"
+    )
+
+    def __repr__(self):
+        return "<User (id=%d, login=%s)>"\
+            % (self.id, self.login)
+
+
+# Auth and registration. Stupid. I know
+def get_user():
+    query_login = request.args.get("login")
+
+    user = db.session.query(User)\
+        .filter_by(login=query_login).first()
+    if user is None:
+        user = User(login=query_login)
+        db.session.add(user)
+        db.session.commit()
+
+    return user
+
+
+@app.route('/api/v1.0/jokes', methods=["GET", "POST"])
+def jokes():
+    cur_user = get_user()
+
+    if request.method == "GET":
+        return str(cur_user.my_jokes)
+
+    new_joke = Joke(content=request.form["joke"])
+    cur_user.my_jokes.append(new_joke)
+    db.session.add(cur_user)
+    db.session.commit()
+
+    return str(new_joke)
+
+
+@app.route("/api/v1.0/jokes/<int:id>", methods=["GET", "DELETE", "PUT"])
+def specific_joke(id):
+    cur_user = get_user()
+
+    for joke in cur_user.my_jokes:
+        if joke.id == id:
+            if request.method == "DELETE":
+                db.session.delete(joke)
+                db.session.commit()
+            elif request.method == "PUT":
+                joke.content = request.form["joke"]
+                db.session.add(joke)
+                db.session.commit()
+
+            return str(joke)
+
+    return {"error": "you have no joke with id %s" % id}
+
+
+@app.route('/api/v1.0/jokes/random', methods=["GET"])
+def random_joke():
+    res = requests.get("https://api.chucknorris.io/jokes/random")
+    data = res.json()
+    new_joke = Joke(content=data["value"])
+
+    cur_user = get_user()
+    cur_user.my_jokes.append(new_joke)
+
+    db.session.add(cur_user)
+    db.session.commit()
+
+    return str(new_joke)
