3d9f07717310b837a0eb3527295cc079e42b9e32
Alex <blohinalexej@mail.ru>
2019-10-05 12:12:21 +0300

    test for jokes view

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.append("..")
 from joke_api import app, db
-from joke_api.models import app, db
+from joke_api.models import User, Joke
 
 
 @pytest.fixture
@@ -20,7 +20,7 @@ def client():
 def test_jokes_random(client):
     url = "/api/v1/jokes/random"
     login, user_id = "FinnTheHuman", 1
-    users_tb = db.session.query(User)
+    users_table = db.session.query(User)
     testcase_num = 5
 
     for i in range(testcase_num):
@@ -31,17 +31,55 @@ def test_jokes_random(client):
         assert res["user_id"] == user_id,\
             "wrong user get a joke"
 
-        assert res["joke"]["id"] = i+1,\
-            "joke have wrong id, %s" % res["joke"]
+        assert res["joke"]["id"] == i+1,\
+            "joke have wrong id"
 
-        joke = res["joke"]["content"]
+        assert res["joke"]["content"] == res["joke"]["content"].strip(),\
+            "joke have trailing whitespaces"
 
-        assert len(joke) > 0,\
-            "blank joke"
-
-        assert joke != strip(joke),\
-            "joke have trailing whitespaces, '%s'" % joke
-
-    user = users_tb.filter(User.login == login)
+    user = users_table.filter(User.login == login).first()
     assert len(user.jokes) == testcase_num,\
         "sent != recieved"
+
+
+def test_jokes(client):
+    url = "/api/v1/jokes"
+    login, user_id = "JakeTheDog", 1
+    users_table = db.session.query(User)
+
+    cases = [
+        "I own the world's worst thesaurus. Not only is it awful, it's awful.",
+        "Parenthood is feeding the mouth that bites you.",
+        "Artificial intelligence is no match for natural stupidity.",
+        "Filthy, stinking rich; well, two out of three ain't bad."
+    ]
+
+    # TEST POST
+    for j in cases:
+        res = client.post(
+            url+"?login=%s" % login,
+            data={"joke": j}
+        ).json
+
+        assert "user" in res and "joke" in res\
+            and "id" in res["user"]\
+            and "login" in res["user"]\
+            and "id" in res["joke"]\
+            and "content" in res["joke"],\
+            "[POST] responce schema is invalid"
+
+        assert res["user"]["id"] == user_id,\
+            "[POST] wrong user get a joke"
+
+    user = users_table.filter(User.login == login).first()
+    assert [j.content for j in user.jokes] == cases,\
+        "[POST] sent != recieved"
+
+    # TEST GET
+    res = client.get(url+"?login=%s" % login).json
+
+    assert [j["content"] for j in res["jokes"]] == cases,\
+        "[GET] wrong collection of jokes returned"
+
+    assert res["user"]["id"] == user_id and res["user"]["login"] == login,\
+        "[GET] wrong user returned"
