6cf4418d1cca6ab2ef23f914a1f5b964be559f6a
Alex <blohinalexej@mail.ru>
2019-10-13 14:15:50 +0300

    env vars and create_tables

--- joke_api/__init__.py
+++ joke_api/__init__.py
@@ -4,12 +4,17 @@ import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-    "DB_URI",
-    default="postgresql://madma:mdma@localhost:5432/app"
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+    "postgresql://%s:%s@localhost:5432/%s" %\
+    (os.getenv("PSQL_USER"), os.getenv("PSQL_PASS"), os.getenv("PSQL_DB"))
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+
+@app.before_first_request
+def create_tables():
+    # db.drop_all()
+    db.create_all()
 
 import joke_api.models
 import joke_api.views
